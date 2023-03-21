f6a8757b0c5ddc57cbc7f8589c49e07fcbc5ffe0
Gorm Labenz <53308156+gormlabenz@users.noreply.github.com>
2023-03-21 19:22:01 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 ref="header" class="text-3xl font-bold underline">{{ st }}</h1>
+</template>
+
+<script setup>
+const st = ref('hello world')
+
+const { width, height } = useWindowSize()
+
+onMounted(() => {
+  console.log(width.value, height.value)
+})
+</script>
